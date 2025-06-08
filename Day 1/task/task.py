0fb13fcbb2b3bb5fc1c104580e4e65508d0655f9
devansh singh <devansh1009@hotmail.com>
2025-06-09 00:31:01 +0530

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,3 @@
+city = input("What's the name of the city you grew up in?")
+petname = input("What's the name of your pet?")
+print(f"Your name is {petname} and is located at {city}.")
---
+++ Day 4/Banker Roulette/task.py
@@ -0,0 +1,4 @@
+import random
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+num = random.randint(0, len(friends)-1)
+print(friends[num])
